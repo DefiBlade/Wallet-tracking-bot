3c704c042db8b2027828a2ab6694e1de4bbc438e
niftyTalent <metastar007@gmail.com>
2024-01-16 01:04:05 +0000

    Add production version for Ethereum and BSC

---
+++ src/components/constant/config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+    NODE_URL: 'wss://bsc-ws-node.nariox.org:443',
+    EXPLORER: 'https://bscscan.com/tx/',
+    EXPLORER_ADDR: 'https://bscscan.com/address/',
+  }
+  
+  export default CONFIG
